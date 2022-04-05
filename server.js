b6b6462885e65830643456709fd19d52eadf6d02
umarnaveed20 <audi.umar20@gmail.com>
2022-04-05 16:36:05 +0500

    CRUD operations and Lookups endpoint fix

--- server.js
+++ server.js
@@ -2,9 +2,28 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', function (req, res) {
     res.send('Welcome to Bulk Reprints API Store')
 });
+
+app.post('/add', function (req, res) {
+    console.log(req.body);
+    res.status(200).send('User Added Successfully!');
+})
+
+app.put('/update/:id', function (req, res) {
+    console.log(req.params.id);
+    console.log(req.body);
+    res.status(200).send('User modified!');
+})
+
+app.delete('/remove/:id', function (req, res) {
+    console.log(req.params.id);
+    res.send('User deleted!')
+})
 
 require('./routes/bulkReprintsRoutes')(app);
 
